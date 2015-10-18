0e3bd3c5c76d52e7b9df530cf9538aeed7e4d459
Ryan Pillsbury <ryanspillsbury90@gmail.com>
2015-10-18 17:19:30 +0200

    Saving before adding authentication module

--- ios/Hale Meditates/SignInViewController.swift
+++ ios/Hale Meditates/SignInViewController.swift
@@ -21,6 +21,19 @@ class SignInViewController: UIViewController, UITextFieldDelegate {
         setUI()
     }
     
+    @IBAction func attemptToSignIn() {
+        
+    }
+    
+    func transitionToTabBarController() {
+        let dest = UIUtil.getViewControllerFromStoryboard("MainTabBarController") as! UITabBarController;
+        let w = self.view.window;
+        UIView.transitionFromView(self.view, toView: dest.view, duration: 1.0, options: UIViewAnimationOptions.TransitionCrossDissolve, completion: {
+            (value: Bool) in
+            w?.rootViewController = dest;
+        });
+    }
+    
     func setUI() {
         
     }
@@ -30,15 +43,17 @@ class SignInViewController: UIViewController, UITextFieldDelegate {
         // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func transitionToSignUpViewController() {
-        let dest = UIUtil.getViewControllerFromStoryboard("SignUpVC") as!UIViewController;
+    func transitionToViewController(dest: UIViewController, transition: UIViewAnimationTransition = UIViewAnimationTransition.FlipFromRight) {
         UIView.beginAnimations("RightFlip", context: nil);
         UIView.setAnimationDuration(0.5);
         UIView.setAnimationCurve(UIViewAnimationCurve.EaseInOut);
-        UIView.setAnimationTransition(UIViewAnimationTransition.FlipFromRight, forView: self.view.superview!, cache: true);
+        UIView.setAnimationTransition(transition, forView: self.view.superview!, cache: true);
         UIView.commitAnimations();
         self.view.window?.rootViewController = dest;
-        
+    }
+    
+    @IBAction func transitionToSignUpViewController() {
+        transitionToViewController(UIUtil.getViewControllerFromStoryboard("SignUpVC") as! UIViewController);
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
